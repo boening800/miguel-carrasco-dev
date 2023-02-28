ec7fa8670ed312a873f0501ec6647b97ed6dee33
boening800 <boening800@gmail.com>
2023-02-27 22:26:13 -0500

    agregando mejoras de diseño

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
+const btnArrowUp = document.getElementById('arrow-up-bottom');
+const socialOptions = document.getElementById('social-options');
+const nav = document.getElementById('nav');
+// console.log(nav.querySelectorAll('ul')[0].classList.add('bg-gray-100'))
+ValidateScrollTop();
 
-// OCULTAR EL MENU SUPERIOR AL HACER SCROLL
-const nav = document.querySelector('nav');
-function watchMenu() {
-    nav.style.top = `-${nav.clientHeight < window.scrollY ?
-            nav.clientHeight : 0
-        }px`;
+function ValidateScrollTop() {
+        if (window.scrollY === 0) {
+            btnArrowUp.classList.add('bottom-[-100px]');
+            socialOptions.classList.remove('left-[-100px]');
+            // nav.classList.remove('bg-gray-100')
+            nav.querySelectorAll('ul')[0].classList.remove('bg-primary')
+        } else {
+            btnArrowUp.classList.remove('bottom-[-100px]');
+            socialOptions.classList.add('left-[-100px]');
+            // nav.classList.add('bg-gray-100')
+            nav.querySelectorAll('ul')[0].classList.add('bg-primary')
+        }
 }
-window.addEventListener('scroll', watchMenu);
+window.addEventListener('scroll', () => { ValidateScrollTop(); });
 
 // CONFIGURACION DE BOTON PARA SUBIR TODO EL SCROLL
-document.getElementById('arrow-up-bottom').addEventListener('click',()=>{
-    window.scroll(0, 0); 
+btnArrowUp.addEventListener('click', () => {
+    window.scroll(0, 0);
 })
 
 // ABRIR MENU
@@ -25,13 +36,13 @@ document.getElementById('exit-menu').addEventListener('click', () => {
     document.getElementById('sidenav').classList.add('right-[-380px]')
 });
 
-window.addEventListener('click',(e)=>{
+window.addEventListener('click', (e) => {
     // CERRAR MENU DESDE AFUERA Y DENTRO
-    if(e.target != true){
+    if (e.target != true) {
         document.getElementById('sidenav').classList.remove('right-0')
         document.getElementById('sidenav').classList.add('right-[-380px]')
     }
-},true);
+}, true);
 
 // ENVIO DE FORMULARIO DE CONTACTO
 const $form = document.querySelector('#form');
@@ -41,7 +52,7 @@ $form.addEventListener('submit', handleSubmit);
 async function handleSubmit(event) {
     console.log('se envcio')
     event.preventDefault();
-    
+
     const form = new FormData(this)
     const response = await fetch(this.action, {
         method: this.method,
@@ -50,7 +61,7 @@ async function handleSubmit(event) {
             'Accept': 'application/json'
         }
     });
-    if(response.ok){
+    if (response.ok) {
         this.reset();
         Swal.fire(
             'Envio satisfactorio',
